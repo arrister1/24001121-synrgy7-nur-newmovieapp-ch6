b653044b3016f0a9ac5038b39d63591ecdbd2bf7
arrister1 <nurultyong2304>
2024-07-06 21:07:55 +0700

    fix error on DetailActivity

--- presentation/src/main/java/com/example/movieapp/presentation/ui/main/DetailActivity.kt
+++ presentation/src/main/java/com/example/movieapp/presentation/ui/main/DetailActivity.kt
@@ -16,14 +16,19 @@ class DetailActivity : AppCompatActivity() {
         _activityDetailBinding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //ambil data dari intent
-        val movies = intent.getParcelableExtra<Movies>("EXTRA_MOVIE")
+        val movies = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            intent.getParcelableExtra("EXTRA_MOVIE",Movie::class.java )
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra<Movie>("EXTRA_MOVIE")
+
+        }
 
         movies?.let {
             binding.tvDetailTitle.text = it.title
-            binding.tvYearContent.text = it.date
+            binding.tvYearContent.text = it.releaseDate
             binding.tvDescContent.text = it.overview
-            Glide.with(this).load("https://image.tmdb.org/t/p/w500/" + it.poster)
+            Glide.with(this).load("https://image.tmdb.org/t/p/w500/" + it.posterPath)
                 .into(binding.imgDetail)
         }
 

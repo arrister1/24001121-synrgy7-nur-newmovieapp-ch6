e88b5b9ab491c4d8ad5f2bb1300d46758ad9fc87
arrister1 <nurultyong2304>
2024-07-10 23:29:23 +0700

    refactor koinModule

--- app/src/main/java/com/example/movieapp/App.kt
+++ app/src/main/java/com/example/movieapp/App.kt
@@ -1,11 +1,11 @@
 package com.example.movieapp
 
 import android.app.Application
-import com.example.movieapp.datas.di.DataModule.dataModule
-import com.example.movieapp.domain.di.DomModule.useCaseModule
-import com.example.movieapp.datas.di.DataModule.networkModule
-import com.example.movieapp.datas.di.DataModule.repositoryModule
-import com.example.movieapp.presentation.di.UiModule.viewModelModule
+import com.example.movieapp.presentation.di.KoinModule.dataModule
+import com.example.movieapp.presentation.di.KoinModule.networkModule
+import com.example.movieapp.presentation.di.KoinModule.repositoryModule
+import com.example.movieapp.presentation.di.KoinModule.useCaseModule
+import com.example.movieapp.presentation.di.KoinModule.viewModelModule
 
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
